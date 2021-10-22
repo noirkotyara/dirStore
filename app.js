77d76d2171c443f229b92014ff3f1a9e9e9403fd
Diana Maliukh <d.maliukh@spd-ukraine.com>
2021-10-22 14:17:49 +0300

    global middleware

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var userRoutes = require("./routes/user.routes");
 var productRoutes = require("./routes/product.routes");
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+  var logger = {
+    request: req.method + ": " + fullUrl,
+    headers: req.headers,
+    body: req.body,
+    params: req.params,
+    query: req.query,
+  };
+  console.log("\n---------Logger----------");
+  console.log(logger);
+
+  next();
+});
 
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
