42396d9c690ab9fae08efe148de4e484fe8389a9
Maliukh <noirkotyara@gmail.com>
2021-11-03 16:42:56 +0200

    resolve threads with product service

--- services/provider.service.js
+++ services/provider.service.js
@@ -4,14 +4,6 @@ var providerModel = require("../models/provider.model");
 
 var createProvider = util.callbackify(providerModel.create).bind(providerModel);
 
-function findProviderById(providerId, callback) {
-  var c = util.callbackify(function () {
-    return providerModel.findOne({ where: { id: providerId } });
-  });
-  return c(callback);
-}
-
 module.exports = {
   createProvider: createProvider,
-  findProviderById: findProviderById,
 };
