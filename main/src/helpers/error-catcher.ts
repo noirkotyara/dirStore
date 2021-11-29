557c6a4a314ef8f26d07c26253c44e97e9139830
Maliukh <noirkotyara@gmail.com>
2021-11-29 11:15:03 +0200

    undo changes in error-catcher

--- main/src/helpers/error-catcher.ts
+++ main/src/helpers/error-catcher.ts
@@ -1,12 +1,8 @@
 import { RESPONSE_CODES } from "message-catcher";
 import { FormedError } from "@types-internal/error/formed-error";
 
-export const errorCatcher = (formedError: FormedError) => {
-  try {
-    const responseCode =
-      formedError.responseCode ?? RESPONSE_CODES.S_ERROR_INTERNAL;
-    throw { responseCode, ...formedError };
-  } catch (e) {
-    console.log(e);
-  }
+export const errorCatcher = (formedError: FormedError): FormedError => {
+  const responseCode =
+    formedError.responseCode ?? RESPONSE_CODES.S_ERROR_INTERNAL;
+  throw { responseCode, ...formedError };
 };
